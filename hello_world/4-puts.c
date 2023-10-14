e36f160b2e1d785dea60a4f2f7e7c35d52b70dee
NDRAKEE <ndrake2985@gmail.com>
2023-10-14 08:02:36 -0700

    script programme in c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	      int x;
+
+	            srand(time(0));
+		          x = rand()  - RAND_MAX / 2;
+
+			        puts("Progaming is like building a multilingual puzzle")
+
+				      return 0;
+}
+
