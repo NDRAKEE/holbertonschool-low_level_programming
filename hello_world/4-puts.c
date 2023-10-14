e9d7963bcabf8f09515dfeed51dc65d71cca869d
NDRAKEE <ndrake2985@gmail.com>
2023-10-14 08:43:58 -0700

    script in c  programme

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-
 /**
  *
- *  * main - entry point "Programming is like building a multilingual
+ *  * main - Prints "Programming is like building a multilingual
  *
  *   *                puzzle, followed by a new line.
  *
