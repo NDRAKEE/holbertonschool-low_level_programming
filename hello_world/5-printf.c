d6277e2a1ea208f3e378951b7187b3ab1314740b
NDRAKEE <ndrake2985@gmail.com>
2023-10-15 08:28:28 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,15 @@
-#include <stdio.h>                                                                                                                                  
-                                                                                                                                                    
-/**                                                                                                                                                 
- * main - prints "with proper grammar, but the outcome is                                                                                       
- *               a piece of art, folowed by new line.                                                                                                   
- *                                                                                                                                                      
- *            Return: 0 Always.                                                                                                                          
- */                                                                                                                                                 int main(void)                                                                                                                                      
-{                                                                                                                                                            printf("with proper grammar, but the outcome is a piece of art,\n");
-			                                                                                                                                                    
-			                        return (0);                                                                                                                 
-}                          
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                   a piece of art,, followed by a new line.
+ *    
+ *       Return: 0 Always.
+ */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+}
