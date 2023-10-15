8ac02ed86238f7152e33fc62b2217d9f53c38de2
NDRAKEE <ndrake2985@gmail.com>
2023-10-15 07:08:18 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,15 @@
-#include <stdio.h>                                                                                                                                                                        
-                                                                                                                                                                                          
-/**                                                                                                                                                                                       
- *  * main - prints "programming is like bulding a multilingual                                                                                                                              
- *   *  puzzle,followed by a new line.                                                                                                                                                        
- *    * Return: Always 0.                                                                                                                                                                      
- *     */                                                                                                                                                                                       
-                                                                                                                                                                                          
-int main(void)                                                                                                                                                                            
-	                                                                                                                                                                                          
-{                                                                                                                                                                                         
-	      puts("\"programing is like bulding a multilingual puzzle");                                                                                                                         
-	                                                                                                                                                                                                
-	            return (0);                                                                                                                                                                         
-		                                                                                                                                                                                              
+#include <stdio.h>
+
+/**
+ *  * main - prints "Programming is like building a multilingual
+ *            puzzle, followed by a new line.
+ *    
+ *       Return: 0 Always.
+ */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 }
