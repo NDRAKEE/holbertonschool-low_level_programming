1e00628649ead8cf7b4c022bef7df91617a5fa96
NDRAKEE <ndrake2985@gmail.com>
2023-10-15 07:58:54 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,19 +1,12 @@
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
-{
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		        return (0);
-
-}
+#include <stdio.h>                                                                                                                                  
+                                                                                                                                                    
+/**                                                                                                                                                 
+ * main - prints "with proper grammar, but the outcome is                                                                                       
+ *               a piece of art, folowed by new line.                                                                                                   
+ *                                                                                                                                                      
+ *            Return: 0 Always.                                                                                                                          
+ */                                                                                                                                                 int main(void)                                                                                                                                      
+{                                                                                                                                                            printf("with proper grammar, but the outcome is a piece of art,\n");
+			                                                                                                                                                    
+			                        return (0);                                                                                                                 
+}                          
