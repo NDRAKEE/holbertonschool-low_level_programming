27eab5ad241d0d8e22ef30ea68d8862d7e82cd32
NDRAKEE <ndrake2985@gmail.com>
2023-10-15 04:32:38 -0700

    programme in c prints

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-
-/**
- *  * main - Prints "with proper grammar, but the outcome is
- *   *                a piece of art," followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	  puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
 }
