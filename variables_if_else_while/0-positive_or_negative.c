d89696282ddcb33c11d7c7527ce0d34cf735f5ce
NDRAKEE <ndrake2985@gmail.com>
2023-10-16 13:14:37 -0700

    0-positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 
- /**
- * main - start
- *
- * Description:
- *
- * Return: 0.
- */
- int main(void)
- {
+  /**
+  * main - start
+  *
+  * Description:
+  *
+  * Return: 0.
+  */
+  int main(void)
+  {
     int n;
 
     srand(time(0));
@@ -24,4 +24,4 @@
         printf("%d is zero\n", n);
 
     return (0);
- }
+    }
