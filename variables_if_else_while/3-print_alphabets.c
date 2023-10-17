63038ab2ae4ff531e8867ead23eb9e4e9db671fb
NDRAKEE <ndrake2985@gmail.com>
2023-10-17 07:00:20 -0700

    3-print_alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,14 @@ int main(void)
    
     char alphabet;
 
-    for (alphabet = 'a'; alphabet <= 'Z' ; alphabet++)
+    for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	      {
+		      {
+
+        putchar(alphabet);
+
+    }
+    for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
     {
 
         putchar(alphabet);
