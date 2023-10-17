60daeeff9bdacc30a928d8eefbb8c622c2d0ce52
NDRAKEE <ndrake2985@gmail.com>
2023-10-17 01:07:44 -0700

    1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+* main - Prints
+*
+* Description: zero positive_or_negative.
+*
+* Return: 0.
+*/
+int main(void)
+{
+        int n, y ;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        if (n >5 )
+                
+        else if (0 = 0)
+                
+        else y = x % 10;
+      printf("The last digit of the number %d is %d \n", n, y);
+
+               
+
+        return (0);
+}
