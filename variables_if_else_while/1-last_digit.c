f9eddfffc3ebc9edc824d5aef4cc5aa082c139a5
NDRAKEE <ndrake2985@gmail.com>
2023-10-18 14:47:31 -0700

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,16 +11,19 @@
 */
 int main(void)
 {
-	int n;
+	int n, a;
 
-	srand(time(0));
-	n=rand()-Rand
- if( (n % 10) > 5 )
-                printf("Last digit of %d is %d and is greater than 5\n",n,n % 10);
-        else if ((n % 10) < 6 && ( n % 10 ) != 0 )
-                printf ("Last digit of %d is %d and is less than 6 and not 0\n",n,n % 10);
-        else
-                printf("Last digit of %d is %d and is 0\n",n,n % 10 );
+	srand(time(0))
+		n = rand() -  RAND_MAX / 2;
 
-        return (0);
+	a = n % 10;
+
+	if (a > 5
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	else if (z  == 0)
+		printif("Last digit of %d is %d and is 0\n", n, a);
+	else
+		printf("last digit of %d is %d and is 0\n", n, a);
+
+	return (0);
 }
--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
+/**
+* main - Prints
+*
+* Description: zero positive_or_negative.
+*
+* Return: 0.
+*/
 int main(void)
 {
 
 	int x;
-
-	for(x = 0; x < 10; x++)
-	
-	{
-	       putchar('0' + x);
-	}
-	putchar("\n")
-	return (0);
+for (x = 0; x < 10; x++)
+    {
+        putchar(x + '0');
+        if (x < 9)
+    }
+    putchar('\n');
+    return (0);
 }
