60d03aa27e59b6350a7e39a12e2c9a515d8c26a6
NDRAKEE <ndrake2985@gmail.com>
2023-10-18 04:19:30 -0700

    5-print_numbers.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -2,22 +2,21 @@
 
 int main(void)
 {
-   
-    char alphabet;
+	char alphabet ;
 
-    for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-		      {
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	 {
 
         putchar(alphabet);
 
     }
-    for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
-    {
+	for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
+	{
 
         putchar(alphabet);
 
     }
 
     putchar('\n');
-    return (0);
+	 return (0);
 }
---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int x;
+
+	for (x = 0; x <10; x++)
+	{
+		putchar(x + '0');
+		if (x < 10)
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	}
+    }
+   putchar('\');
+   return (0);
+}
