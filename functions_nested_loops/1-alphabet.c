40119b4521013eb185a647dcf0a693a8169b8d5c
NDRAKEE <ndrake2985@gmail.com>
2023-10-20 05:42:47 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+include "main.h"
+
+/**
+ * main -  prints _putchar,
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar("a")
+	_putchar("b")
+	_putchar("c")
+	_putchar("d")
+	_putchar("e")
+	_putchar("f")
+	_putchar("j")
+	_putchar("h")
+	_putchar("i")
+	_putchar("g")
+	_putchar("k")
+	_putchar("l")
+	_putchar("m")
+	_putchar("o")
+	_putchar("p")
+	_putchar("q")
+	_putchar("r")
+	_putchar("s")
+	_putchar("t")
+	_putchar("u")
+	_putchar("v")
+	_putchar("w")
+	_putchar("x")
+	_putchar("y")
+	_putchar("z")
+
+	putchar("n/")
+	return : (0)
+
