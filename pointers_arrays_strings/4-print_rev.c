1d6d380a517e4a56ade86e1123275134ce2a3648
NDRAKEE <ndrake2985@gmail.com>
2023-10-27 15:30:21 -0700

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - putchar caractere
+ * @s: i putchar
+*/
+void print_rev(char *s);
+{
+	char*s = len
+
+	for ( i = 0, i <  len, i++)
+	{
+
+		_putchar(*s[i]);
+
+	}
+
+	for (i = len-1; i > 0; i--)
+	{
+		_putchar(s[i];
+	}
+
+	_putchar('\n');
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-void reset_to_98(int *n);
+void print_rev(char *s);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+void print_rev(char *s);
