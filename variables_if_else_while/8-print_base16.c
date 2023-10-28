6e197f64cd8c82a07ac516fd594e26c75adf8c47
NDRAKEE <ndrake2985@gmail.com>
2023-10-28 15:42:35 -0700

    putchar fonction

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 15; num++)
+	{
+
+		char ( "%x", num);
+	}
+	char("\n");
+	return (0);
+}
